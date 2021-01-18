7a3d79850536afeba61bd7eb6205ca8096e3ace6
Omar Diab <me@omardiab.com>
2021-01-18 16:52:59 +0900

    less in-your-face heading styles

--- src/styles/global/text.ts
+++ src/styles/global/text.ts
@@ -1,21 +1,21 @@
 import { css } from "@emotion/react";
 import { mediaQueries } from "src/styles/mediaQueries";
-
-const textBlockCss = css`
-  :not(:last-child) {
-    margin-bottom: 1.2rem;
-  }
-`;
+import { palette } from "src/styles/palette";
 
 const boldCss = css`
   font-weight: 700;
 `;
 
 export const globalTextStyles = css`
-  p {
-    font-size: 1.2rem;
+  p,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: 1.25rem;
     line-height: 1.5;
   }
+
   h1 {
     font-size: 5rem;
     @media (max-width: ${mediaQueries.sizes.phoneLarge}) {
@@ -26,38 +26,28 @@ export const globalTextStyles = css`
   }
 
   h2 {
-    font-size: 3.5rem;
-    @media (max-width: ${mediaQueries.sizes.phoneLarge}) {
-      font-size: 2.5rem;
-    }
-    ${boldCss};
+    font-size: 1.5 rem;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+  }
+
+  h1,
+  h2 {
+    line-height: 1.25;
   }
 
   h3 {
-    font-size: 2.5rem;
-    @media (max-width: ${mediaQueries.sizes.phoneLarge}) {
-      font-size: 2rem;
-    }
     ${boldCss};
   }
 
-  h4 {
-    font-size: 2rem;
-    @media (max-width: ${mediaQueries.sizes.phoneLarge}) {
-      font-size: 1.6rem;
-    }
-    ${boldCss};
-  }
-
-  h5 {
-    font-size: 1.4rem;
-    ${boldCss};
-  }
-
+  h4,
+  h5,
   h6 {
-    font-size: 1.4rem;
+    color: ${palette.secondaryText};
   }
+`;
 
+export const compositionTextCss = css`
   p,
   h1,
   h2,
@@ -65,6 +55,8 @@ export const globalTextStyles = css`
   h4,
   h5,
   h6 {
-    ${textBlockCss};
+    &:not(:last-child) {
+      margin-bottom: 1.2rem;
+    }
   }
 `;
