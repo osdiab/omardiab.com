441eedc5a02dd9c7c5848c8c61fada3c851b5560
Omar Diab <me@omardiab.com>
2022-08-25 16:40:43 +0900

    put all global styles in global style file

--- src/styles/global/index.ts
+++ src/styles/global/index.ts
@@ -1,10 +1,14 @@
+import "modern-normalize/modern-normalize.css";
 import { globalCss } from "@stitches/react";
 import { palette } from "../palette";
 
 export const appStyles = globalCss({
   "html, body": {
+    fontFamily:
+      "-apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif",
     minHeight: "100vh",
     color: palette.bodyText,
     background: palette.background,
   },
+  "*": { margin: 0, padding: 0 },
 });
