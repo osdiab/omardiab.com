213f257ca7bc6b2df67f8c3aadd77009ce4505b8
Omar Diab <me@omardiab.com>
2022-09-12 12:34:58 +0900

    hover style for links

--- src/styles/hyperlink.ts
+++ src/styles/hyperlink.ts
@@ -4,5 +4,8 @@ import { palette } from "./palette";
 export const hyperlinkRawCss: CSS = {
   color: palette.primary,
   textDecoration: "underline",
+  "&:hover,&:focus": {
+    color: palette.primaryHighlight,
+  },
 };
 export const hyperlinkCss = css(hyperlinkRawCss);
