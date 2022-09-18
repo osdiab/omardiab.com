be66df9b42da76ed2b19abc9bc2d6890cd38d4a8
Omar Diab <me@omardiab.com>
2022-09-18 19:04:35 +0900

    use theme to style header, thinner padding on mobile

--- src/layouts/header.css.ts
+++ src/layouts/header.css.ts
@@ -1,9 +1,14 @@
-import { style } from "@vanilla-extract/css";
+import { createTheme, style } from "@vanilla-extract/css";
+import { textSizeObjects } from "../styles/text";
 import { themeVars } from "../styles/theme.css";
+import { calc } from "@vanilla-extract/css-utils";
+import { textCss } from "../styles/text.css";
 
+export const [headingTheme, headingVars] = createTheme(textSizeObjects.xxl);
+export const headingTextCss = style([headingVars, textCss({ weight: "bold" })]);
 export const avatarCss = style({
-  width: "150px",
-  height: "150px",
+  height: calc.multiply(headingVars.lineHeight, 3),
+  aspectRatio: 1,
   objectFit: "cover",
 });
 export const nameHighlightCss = style({
--- src/styles/page-section.css.ts
+++ src/styles/page-section.css.ts
@@ -1,9 +1,13 @@
 import { style } from "@vanilla-extract/css";
+import { mediaVariants } from "./media-queries";
 import { spacing } from "./spacing";
 
 export const pageSectionCss = style({
-  padding: `${spacing.l} ${spacing.xxl}`,
+  padding: `${spacing.m}`,
   maxWidth: "1000px",
   width: "100%",
   margin: "0 auto",
+  "@media": {
+    [mediaVariants.tablet]: { padding: `${spacing.l} ${spacing.xxl}` },
+  },
 });
