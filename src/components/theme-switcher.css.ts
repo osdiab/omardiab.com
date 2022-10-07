963da0c2b2a0d8a3921ab519d93dff740ce49300
Omar Diab <me@omardiab.com>
2022-10-07 11:45:12 +0900

    better click target

--- src/components/theme-switcher.css.ts
+++ src/components/theme-switcher.css.ts
@@ -6,23 +6,23 @@ import { themeVars } from "../styles/theme.css";
 
 export const switcherCss = style([
   {
-    borderRadius: "100%",
-    background: themeVars.background,
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderColor: themeVars.border.light,
     position: "fixed",
     left: spacing.m,
     bottom: spacing.m,
-    padding: spacing.xs,
-    lineHeight: "1em",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
   },
 ]);
 export const switcherButtonCss = style({
-  display: "block",
+  borderRadius: "100%",
+  background: themeVars.background,
+  borderWidth: "1px",
+  borderStyle: "solid",
+  borderColor: themeVars.border.light,
+  lineHeight: "1em",
+  padding: spacing.xs,
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 });
 export const switcherIconCss = style([
   textCss({ size: "l" }),
