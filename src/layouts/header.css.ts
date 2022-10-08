14dc47a06b27e895064bdf98fb42d4bde179088f
Omar Diab <me@omardiab.com>
2022-10-08 22:05:31 +0900

    rework how spacing is handled, tighter header/homepage spacing

--- src/layouts/header.css.ts
+++ src/layouts/header.css.ts
@@ -1,5 +1,6 @@
 import { style } from "@vanilla-extract/css";
 import classNames from "classnames";
+import { spacing } from "../styles/spacing";
 import { stackCss } from "../styles/spacing.css";
 import { textCss } from "../styles/text.css";
 import { themeVars } from "../styles/theme.css";
@@ -10,13 +11,15 @@ export const headerCss = style({
   borderBottom: "1px solid",
   borderBottomColor: themeVars.border.light,
   background: themeVars.background,
+  paddingTop: spacing.m,
+  paddingBottom: spacing.m,
   zIndex: 100,
 });
 
 export const headerNavCss = classNames(
   textCss({ size: "xs" }),
-  stackCss({ gap: "s", flexDirection: "row", flexWrap: "wrap" }),
-  style({ marginLeft: "auto" })
+  stackCss({ flexDirection: "row", flexWrap: "wrap", justifyContent: "end" }),
+  style({ marginLeft: "auto", gap: `${spacing.xxxs} ${spacing.m}` })
 );
 
 const hoverCss = { color: themeVars.primary.normal };
--- src/styles/page-section.css.ts
+++ src/styles/page-section.css.ts
@@ -3,11 +3,15 @@ import { mediaVariants } from "./media-queries";
 import { spacing } from "./spacing";
 
 export const pageSectionCss = style({
-  padding: `${spacing.m}`,
+  paddingLeft: `${spacing.m}`,
+  paddingRight: `${spacing.m}`,
   maxWidth: "1000px",
   width: "100%",
   margin: "0 auto",
   "@media": {
-    [mediaVariants.tablet]: { padding: `${spacing.l} ${spacing.xxl}` },
+    [mediaVariants.tablet]: {
+      paddingLeft: spacing.xxl,
+      paddingRight: spacing.xxl,
+    },
   },
 });
